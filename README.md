f0d49417711e89459104595d3119d82996f31ea0
yumusb <yumusb@foxmail.com>
2021-02-01 22:30:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+# DNSLog-Platform-Golang
+
+相信DNSLog平台已经是安全从业者的标配。而公开的DNSLOG平台域名早已进入流量监控设备的规则库。同时也有隐私问题值得关注。于是撸了（凑了）一个一键搭建Dnslog平台的golang版本。可以使用其一键搭建自己的Dnslog平台。由于是作者的第一个golang程序，难免会有一些小问题。不过 他真的是一键！
+
+## 部署
+
+1. 选择你喜欢的方式下载本目录下的main.go文件
+
+   不多说。
+
+2. 创建数据存放目录 
+
+   `mkdir /tmp/dnslog/`
+
+3. 决定是否开放公网访问？
+
+   倒数第四行`http.ListenAndServe("localhost:8000", nil) `，这样写的话只能通过localhost进行访问，墙裂建议不要修改，而后通过中间件反向代理后对外开放，方便做访问控制、日志管理等。如果想直接对外开放的话可以修改为:`http.ListenAndServe(":8000", nil)`
+
+4. 域名准备
+
+   不多说，做好NS指向即可。
+
+5. 运行
+
+   `go run main.go yourdomain` 可先在shell前台运行，看功能是否正常使用。如果没问题的话 直接 `nohup go run main.go yourdomain &`
+
+   （第一次运行需要到github拖miekg/dns库，所以需要你的服务器能上github）
+
+## 使用
+
+直接web访问进行使用。或者使用api接口。只有两个URI，
+
+1. /new_gen
+
+   生成随机的八位字符，组合到domain中并返回。当然也可以本地生成，没做限制，无所谓。
+
+2. /八位字符
+
+   第一步生成的八位字符可以直接访问，可以获取到相关的DNS解析记录。返回为null或者正常数据的JSON形式。
+
+   
+
+PS：当然，你也可以通过 go build 打包成可执行文件进行跨平台或者离线运行。这都依赖于go的特性。
+
+## 其他
+
+感谢高学长给予的大力帮助：https://github.com/netchiso
+
---
+++ main.go
@@ -0,0 +1,298 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strconv"
+	"time"
+	"fmt"
+	"net/http"
+	"strings"
+	"os"
+	"sync"
+	"math/rand"
+	"github.com/miekg/dns"
+	"io/ioutil"
+	"encoding/json"
+)
+var ip string
+
+var tmplogdir = "/tmp/dnslog/"
+var letters = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
+var topDomain string
+
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+func GetDnslog(id string) string {
+    content := "content"
+    path := tmplogdir+id
+    if(Exists(path)){
+        file, _ := os.Open(path)
+        defer file.Close()
+        tmpcontent , _ := ioutil.ReadAll(file)
+        content = string(tmpcontent)
+        res := make(map[int]map[string]string)
+        data := make(map[string]string)
+        i := 0
+        y := []string{}
+        for _,x := range strings.Split(content,"\n"){
+            if(len(x)>5){
+                y = strings.Split(x,"|")
+                data["time"]=y[0]
+                data["ip"]=y[1]
+                data["subdomain"]=y[2]
+                res[i] = data
+                i++
+            }
+            
+        }
+        enc,_ := json.Marshal(res)
+        content = string(enc)
+    }else{
+        content = "null"
+    }
+    
+    return string(content)
+    
+}
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+
+	res := "Hello World"
+	if(len(r.URL.Path)==9){
+	    w.Header().Set("Content-Type", "application/json")
+	    res = GetDnslog(r.URL.Path)
+	}else if(r.URL.Path=="/new_gen"){
+	    rand.Seed(time.Now().UnixNano())
+	    key := randSeq(8)
+        res = key+"."+topDomain
+	}else{
+	    res = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>DNSLog Platform</title>
+<meta name="keywords" content="dnslog，dnslog平台"/>
+<meta name="description" content="一个无需注册就可以快速使用的DNSLog平台"/>
+</head>
+<body>
+<div id=header style="text-align:center">
+<hr style=" height:2px;border:none;border-top:2px dashed #87CEFA;"/><br>
+</div>
+<script>
+function GetDomain()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("myDomain").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","/new_gen?t="+ Math.random(),true);
+	xmlhttp.send();
+}
+
+function GetRecords()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			var abc = xmlhttp.responseText;
+			obj = JSON.parse(abc);
+			if(obj==""){
+				ktable = "<tr bgcolor=\"#ADD3EF\"><th width=\"50%%\">DNS Query Record</th><th width=\"25%%\">IP Address</th><th width=\"25%%\">Created Time</th></tr><td colspan=\"3\" align=\"center\">No Data</td>";
+				document.getElementById("myRecords").innerHTML = ktable;
+			}else{
+			table = "<tr bgcolor=\"#ADD3EF\"><th width=\"50%%\">DNS Query Record</th><th width=\"25%%\">IP Address</th><th width=\"25%%\">Created Time</th></tr>";
+            for (var obj1 in obj)
+            {
+				
+				table = table + "<tr><td>"+obj[obj1]['subdomain']+"</td><td>"+obj[obj1]['ip']+"</td>"+"<td>"+obj[obj1]['time']+"</td></tr>";
+            }
+			document.getElementById("myRecords").innerHTML = table;
+		}
+		}
+	}
+	xmlhttp.open("GET","/"+document.getElementById("myDomain").innerHTML.split(".")[0]+"?t="+Math.random(),true);
+	xmlhttp.send();
+}
+</script>
+<div id="content" style="text-align:center;">
+<button type="button" onclick="GetDomain()">Get SubDomain</button>
+<button type="button" onclick="GetRecords()">Refresh Record</button><br><br>
+<div id="myDomain">&nbsp;</div><br>
+<center><table id="myRecords" width=700 border="0" cellpadding="5" cellspacing="1" bgcolor="#EFF3FF" style="word-break:break-all; word-wrap:break-all;">
+  <tr bgcolor="#ADD3EF"><th width="50%%">DNS Query Record</th><th width="25%%">IP Address</th><th width="25%%">Created Time</th>
+  </tr>
+  <tr>
+    <td colspan="3" align="center">No Data</td>
+  </tr>
+</table></center>
+</div>
+</body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id=footer>
+<hr style=" height:2px;border:none;border-top:2px dashed #87CEFA;"/><br>
+<center><span style="color:#ADD3EF">Copyright © 2019 DNSLog Platform All Rights Reserved.</span></center>
+</div>
+</html>`
+	    
+	}
+	
+	fmt.Fprintf(w,res)
+ }
+type Tunnel struct {
+	Messages       chan string
+	cancel         chan struct{}
+	fgListsLock    sync.Mutex
+	topDomain      string
+	domains        chan string
+	maxMessageSize int
+}
+
+
+
+func NewTunnel(topDomain string,expiration time.Duration, maxMessageSize int) *Tunnel {
+	tun := &Tunnel{
+		Messages:       make(chan string, 256),
+		cancel:         make(chan struct{}),
+		topDomain:      topDomain,
+		domains:        make(chan string, 256),
+		maxMessageSize: maxMessageSize,
+	}
+	go tun.listenDomains()
+	return tun
+}
+
+func (tun *Tunnel) Close() {
+	close(tun.cancel)
+}
+
+func (tun *Tunnel) listenDomains() {
+	for {
+		select {
+		case <-tun.cancel:
+			return
+		case domain := <-tun.domains:
+			func() {
+				tun.fgListsLock.Lock()
+				defer tun.fgListsLock.Unlock()
+				idkeys := strings.Split(domain[0:len(domain)-len(tun.topDomain)-1],".")
+				idkey := idkeys[len(idkeys)-1]
+				log.Print(idkey)
+				if(len(idkey)==8){
+    				fd,_:=os.OpenFile(tmplogdir+idkey,os.O_RDWR|os.O_CREATE|os.O_APPEND,0644)
+    				fd_time:=time.Now().Format("2006-01-02 15:04:05");
+    				fd_content:=strings.Join([]string{fd_time,"|",ip,"|",domain,"\n"},"")
+    				log.Print(fd_content)
+                    buf:=[]byte(fd_content)
+                    fd.Write(buf)
+                    fd.Close()
+				    
+				}
+				
+			}()
+		}
+	}
+}
+
+
+// ServeDNS handles DNS queries, records them, and replies with a CNAME to blackhole-1.iana.org.
+func (tun *Tunnel) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	if len(r.Question) < 1 {
+		return
+	}
+	ip = w.RemoteAddr().String()
+	domain := r.Question[0].Name
+	if r.Question[0].Qtype == dns.TypeA {
+		tun.domains <- domain
+	}
+
+	m := &dns.Msg{}
+	m.SetReply(r)
+	m.Answer = []dns.RR{
+		&dns.CNAME{
+			Hdr:dns.RR_Header{Name: domain, Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 0},
+			Target: "blackhole-1.iana.org.",
+		},
+	}
+	err := w.WriteMsg(m)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+
+	port := flag.Int("port", 53, "port to run on")
+	expiration := flag.Int("expiration", 60, "seconds an incomplete message is retained before it is deleted")
+	maxMessageSize := flag.Int("maxMessageSize", 5000, "maximum encoded size (in bytes) of a message")
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("tunnel accepts exactly one argument for the top domain")
+	}
+	topDomain = dns.Fqdn(flag.Arg(0))
+    expirationDuration := time.Duration(*expiration) * time.Second
+	tun := NewTunnel(topDomain, expirationDuration, *maxMessageSize)
+	dns.Handle(topDomain, tun)
+	go func() {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "udp"}
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to set udp listener %s\n", err.Error())
+		}
+	}()
+	go func() {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "tcp"}
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to set tcp listener %s\n", err.Error())
+		}
+	}()
+	
+	http.HandleFunc("/", HelloHandler)
+	http.ListenAndServe("localhost:8000", nil)
+    log.Print("Begin Http Server")
+	select {} // block foreve
+	
+}
